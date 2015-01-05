01e32ae8f434c5648e7cc2eab6fcd605e1045cb2
Aurelien Saussay <aurelien.saussay@gmail.com>
2015-01-05 17:57:47 +0100

    Added article text extraction

--- livebuzz/spiders/lemonde.py
+++ livebuzz/spiders/lemonde.py
@@ -2,6 +2,8 @@ import scrapy
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
 
+import lxml
+
 from readability.readability import Document
 
 from livebuzz.items import LivebuzzItem
@@ -12,15 +14,16 @@ class LeMondeSpider(CrawlSpider):
     start_urls = ["http://www.lemonde.fr/"]
 
     rules = (
-        Rule(LinkExtractor(allow=('html$', )), callback='parse_link'),
+        Rule(LinkExtractor(allow=('html$', ), deny=('sitemap', )), callback='parse_link'),
     )
 
     def parse_link(self, response):
         if len(response.css('h1').xpath('text()')) > 0:
             item = LivebuzzItem()
-            item['title'] = response.css('h1').xpath("text()").extract()[0]
+            article = Document(response.body).summary()
+            item['title'] = response.css('h1').xpath("text()").extract()[0].strip()
             item['url'] = response.url
-            item['content'] = "pouet"
+            item['content'] = lxml.html.document_fromstring(article, parser = lxml.html.HTMLParser(encoding='utf-8')).text_content()
             item['keywords'] = ["pouet"]
             return item
         # response.css("a[href^='/'][href$='html']").xpath("@href").extract()
